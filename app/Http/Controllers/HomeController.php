33bab83c910e6dceda5324ba48b89aee00d7322b
mokim <tamerp102@gmail.com>
2021-01-13 10:50:54 +0100

    home db

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Category;
+use App\Client;
+use App\Order;
+use App\Product;
+use App\User;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\DB;
 
 class HomeController extends Controller
 {
@@ -23,6 +30,18 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        $categories_count = Category::count();
+        $products_count = Product::count();
+        $clients_count = Client::count();
+        $users_count = User::whereRoleIs('admin')->count();
+
+        $sales_data = Order::select(
+            DB::raw('YEAR(created_at) as year'),
+            DB::raw('MONTH(created_at) as month'),
+            DB::raw('SUM(total_price) as sum')
+        )->groupBy('month')->get();
+
+
+        return view('home', compact('categories_count', 'products_count', 'clients_count', 'users_count', 'sales_data'));
     }
 }
